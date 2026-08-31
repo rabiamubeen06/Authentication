85628dd78c043f33faa648d71616968fc8d6020a
Rabia Mubeen <mubeenrabia567@gmail.com>
2026-08-31 09:45:10 -0700

    added Middlware

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,9 +2,11 @@
 import axios from "axios"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
+import { useState } from "react"
 import toast from "react-hot-toast"
 export default function ProfilePage(){
     const router=useRouter();
+    const [data,setData]=useState("nothing");
 const logout=async()=>{
     try {
         const response=await axios.get("/api/users/logout");
@@ -16,11 +18,23 @@ const logout=async()=>{
         toast.error(error.message);
     }
 }
+const getUserDetails=async()=>{
+    const res=await axios.get("/api/users/me");
+    setData(res.data.data._id);
+    
+}
+
 
     return (
         <div className="flex flex-col items-center *:justify-center min-h-screen py-2">
             <h1>Profile</h1>
+            <h2>{data==='nothing'?"Nothing":
+                <Link href={`/profile/${data}`}>{data}</Link>
+                }</h2>
             <button>Logout</button>
+            <button onClick={getUserDetails}>
+                GetUserDetails
+            </button>
 
         </div>
     )
